abb600429c7fa0555a4ad50ff876064cce5905d9
Wafiakmal <104295829+wafiakmal@users.noreply.github.com>
2022-11-27 23:52:09 +0000

    add python file for sentiment analysis using SIA part 1

--- Makefile
+++ Makefile
@@ -9,9 +9,14 @@ format:
 
 lint:
 	# pylint
+	pylint --disable=R,C *.py mylib/*.py
 
 test:
 	# test
+	python -m pytest -vv --cov=mylib --cov=main test_*.py
+
+build:
+	# build container
 
 deploy:
 	# deploy
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,8 +1,41 @@
-import wikipedia
+#!/bin/usr/env python
+
+import os
+import praw
+import pandas as pd
+import fire
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+import nltk
+nltk.download('vader_lexicon')
+
+reddit_read_only = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+        user_agent=os.getenv("REDDIT_CLIENT_USER_AGENT"),)
+
+class RedditScraper:
+    def get_headlines(self, subred):
+        """Get headlines from a subreddit."""
+        headlines = set()
+        subredditx = reddit_read_only.subreddit(subred).new(limit=3)
+        for submission in subredditx:
+            headlines.add(submission.title)
+        return list(headlines)
+    
+    def get_sentiment(self, subred):
+        """Get sentiment from a subreddit."""
+        sia = SIA()
+        results = []
+        subredditx = reddit_read_only.subreddit(subred).new(limit=3)
+        for submission in subredditx:
+            pol_score = sia.polarity_scores(submission.title)
+            pol_score['headline'] = submission.title
+            results.append(pol_score)
+        df = pd.DataFrame.from_records(results)
+        df.to_csv('sentiment.csv', index=False)
 
 
-def wiki(name="War Goddess", length=1):
-    """This is a wikipedia fetcher"""
 
-    my_wiki = wikipedia.summary(name, length)
-    return my_wiki
+if __name__ == "__main__":
+    reddit = RedditScraper()
+    fire.Fire(reddit)
