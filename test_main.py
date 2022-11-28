a0e4ee6cf0bea5be41f19be263c2bf2fec0ddeda
Wafiakmal <104295829+wafiakmal@users.noreply.github.com>
2022-11-28 04:13:28 +0000

    deleting test subreddit

--- test_main.py
+++ test_main.py
@@ -9,7 +9,3 @@ def test_read_main():
     assert response.json() == {
        "message": "This api will show the sentiment of a subreddit of your choice."
     }
-
-def test_read_subreddit():
-    response = client.get("/top_three/soccer")
-    assert response.status_code == 200
